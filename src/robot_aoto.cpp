1084c182c2282b13a3b1081c42428d15f6befe0c
hui <hui101902012outlook.com>
2025-02-26 19:40:40 +0800

    robot.servo2

--- src/robot_aoto.cpp
+++ src/robot_aoto.cpp
@@ -735,19 +735,19 @@ void PART1() {
         robot.servo[0]->setAngle(30);  // 爪子张开（框抬起）的角度
         robot.servo[1]->setAngle(-30); // 爪子张开（框抬起）的角度
         // delay(20);// 延时20ms
-        SetSpeed(0.6, 0, 0, 0.45); // 向前冲一段
+        SetSpeed(0.7, 0, 0, 0.45); // 向前冲一段
         // delay(20);// 延时20ms
         robot.servo[0]->setAngle(-16); // 爪子合拢（框放下）的角度
         robot.servo[1]->setAngle(13);  // 爪子合拢（框放下）的角度
         delay(1000);                   // 延时300ms
-        SetSpeed(-0.6, 0, 0, 0.2);     // 向后冲一段
+        SetSpeed(-0.6, 0, 0, 0.3);     // 向后冲一段
         delay(1000);                   // 延时300ms
         robot.servo[2]->setAngle(-70); // 云台舵机向下
         delay(1000);
         pd2 = certain_ball(&robot, b, 1); // 检测里面的球是否不为对方颜色
         if (pd2 == 1) {
           mine = mine + 1;               // mine次数叠加(找到的次数叠加)
-          robot.servo[2]->setAngle(-30); // 云台舵机向上（抬头）
+          robot.servo[2]->setAngle(-26); // 云台舵机向上（抬头）
           // delay(100);                   // 延时100ms
           break;
         } else if (pd2 == 2) {
@@ -756,10 +756,10 @@ void PART1() {
           SetSpeed(-0.25, 0, 0, 0.4);    // 向后移一段
           robot.servo[0]->setAngle(-16); // 爪子合拢（框放下）的角度
           robot.servo[1]->setAngle(13);  // 爪子合拢（框放下）的角度
-          robot.servo[2]->setAngle(-30); // 云台舵机向上（抬头）
+          robot.servo[2]->setAngle(-26); // 云台舵机向上（抬头）
           // delay(100);                    // 延时100ms
         } else {
-          robot.servo[2]->setAngle(-30); // 云台舵机向上（抬头）
+          robot.servo[2]->setAngle(-26); // 云台舵机向上（抬头）
           // delay(100);                   // 延时100ms
         }
       }
